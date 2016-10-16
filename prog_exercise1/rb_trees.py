670bab9ac88ed2238798681786dda1cfcf80d6b1
Anupam Sobti <anupamsobti@gmail.com>
2016-10-16 22:36:13 +0530

    Fresh start to rb trees

--- prog_exercise1/rb_trees.py
+++ prog_exercise1/rb_trees.py
@@ -1,81 +1,44 @@
 #!/usr/bin/python3
-import networkx as nx
-from matplotlib import pyplot as plt
 
 class rbTree():
-    def __init__(self,data,color,parent=None,leftChild=None,rightChild=None):
+    def __init__(self,data,color,parent=None,leftChild=None,rightChild=None,leaf=False):
         self.data = data
         self.color = color
         self.parent = parent
         self.leftChild = leftChild
         self.rightChild = rightChild
+        self.leaf = leaf
 
     def insertNode(self,x):
-        y = self.searchNode(x)   #Returns the node where the search ended
-        if y.parent == None:
-            if x <= y.data:
-                y.leftChild = rbTree(x,'b')
-                y.leftChild.parent = y
-            else:
-                y.rightChild = rbTree(x,'b')
-                y.rightChild.parent = y
+        isPresent,y = self.searchNode(x)   #Returns the node where the search ended
+        if not isPresent:
+            if y.parent == None:    #The only leaf
+                newLeafNode = rbTree(x,leaf=True,color = 'b')
+                if x < y.data:
+                    newInternalNode = rbTree(x,leftChild = newLeafNode,rightChild = y,color = 'b')
+                else:
+                    newInternalNode = rbTree(x,leftChild = y,rightChild = newLeafNode,color = 'b')
 
+                newLeafNode.parent = newInternalNode
 
     def searchNode(self,x):
-        return self
+        if x == self.data and self.leaf == True:
+            return True,self
+        elif x <= self.data:
+            if self.leftChild == None or self.leaf:
+                return False,self
+            else:
+                return self.leftChild.searchNode(x)
+        else:   #x > self.data
+            if self.rightChild == None or self.leaf:
+                return False,self
+            else:
+                return self.rightChild.searchNode(x)
 
     def deleteNode(self,x):
         pass
 
-
-
-def createDiagram(tree):
-
-    def populateNode(G,treeNode):
-        G.add_node(treeNode.data)
-        if treeNode.leftChild != None:
-            populateNode(G,treeNode.leftChild)
-            G.add_edge(treeNode.data,treeNode.leftChild.data)
-        if treeNode.rightChild != None:
-            populateNode(G,treeNode.rightChild)
-            G.add_edge(treeNode.data,treeNode.rightChild.data)
-
-    def hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, 
-                      pos = None, parent = None):
-        '''If there is a cycle that is reachable from root, then this will see infinite recursion.
-           G: the graph
-           root: the root node of current branch
-           width: horizontal space allocated for this branch - avoids overlap with other branches
-           vert_gap: gap between levels of hierarchy
-           vert_loc: vertical location of root
-           xcenter: horizontal location of root
-           pos: a dict saying where all nodes go if they have been assigned
-           parent: parent of this branch.'''
-        if pos == None:
-            pos = {root:(xcenter,vert_loc)}
-        else:
-            pos[root] = (xcenter, vert_loc)
-        neighbors = G.neighbors(root)
-        #if parent != None:
-        #    neighbors.remove(parent)
-        if len(neighbors)!=0:
-            dx = width/len(neighbors) 
-            nextx = xcenter - width/2 - dx/2
-            for neighbor in neighbors:
-                nextx += dx
-                pos = hierarchy_pos(G,neighbor, width = dx, vert_gap = vert_gap, 
-                                    vert_loc = vert_loc-vert_gap, xcenter=nextx, pos=pos, 
-                                    parent = root)
-        return pos
-
-    G = nx.DiGraph()
-    populateNode(G,tree)
-    plt.title("NX Tree")
-    pos = hierarchy_pos(G,5)
-    nx.draw_networkx(G,pos)
-    plt.show()
-
 t = rbTree(5,'b')
 t.insertNode(3)
 t.insertNode(7)
-createDiagram(t)
+print(t.searchNode(3))
